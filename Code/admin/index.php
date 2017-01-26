1cdaee7c47d96415f7af80ea1de25b4c18ba1182
Timbo Cole <timbo_cole@yahoo.co.uk>
2017-01-26 01:58:15 +0000

    Began working on the CMS Front End

---
+++ Code/admin/index.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head>
+        <title>Administrator Login</title>
+        <link rel='stylesheet' type='text/css' href='../css/bootstrap.css'>
+        <link rel="stylesheet" type="text/css" href="styles.css"
+        <script src='../js/jquery-3.1.1.min.js'></script>
+        <script src='../js/bootstrap.min.js'></script>
+    </head>
+    <body>
+        <div class="box">
+            <h1>1234</h1>
+        </div>
+    </body>
+</html>
