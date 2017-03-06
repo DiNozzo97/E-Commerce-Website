24f4cf13f954b45db2d65516bbd1f0617cae9927
yusuf1239 <yusufe9@hotmail.co.uk>
2017-03-06 11:42:03 +0000

    .

--- Code/reg.php
+++ Code/reg.php
@@ -1,110 +1,103 @@
 <?php
-require '../vendor/autoload.php'; // Import the MongoDB library
-    
-session_start(); // Start the PHP Session
+session_start();
 
-$client = new MongoDB\Client("mongodb://localhost:27017"); // Connect to the MongoDB server
+if($_POST['submit']){
+    $ScName=strip_tags($_POST['ScName']);
+    $fname=strip_tags($_POST['fname']);
+    $lname=strip_tags($_POST['lname']);
+    $email=strip_tags($_POST['email']);
+    $password=strip_tags($_POST['password']);
+    $password2=strip_tags($_POST['password2']);
 
-$collection = $client->movie_box->customers; // Select the database and collection
+    $error = array();
 
-if ($_POST['type'] == "fetch") { // If the 'fetch' type message is posted then
+        if(empty($email) or !filter_var($email,FILTER_SANITIZE_EMAIL))
+        {
+          $error[] = "Email id is empty or invalid";
+        }
+        if(empty($password)){
+          $error[] = "Please enter password";
+        }
+        if(empty($password2)){
+          $error[] = "Please enter Confirm password";
+        }
+        if($password != $password2){
+           $error[] = "Password and Confirm password are not matching";
+        }
+        if(empty($fname)){
+            $error[] = "Enter first name";
+        }
+        if(empty($lname)){
+            $error[] = "Enter last name";
+        }
 
+        if(count($error == 0)){
+            //database configuration
+            $host = 'localhost';
+            $database_name = 'mongo1';
+            $database_user_name = '';  
+            $database_password = '';  
 
-	$userDetails = []; // Create an array to store the data
+             $connection=new Mongo('mongodb://localhost:27017');
 
-	// Add each piece of data in to the associative array
-	$userDetails['firstNameEditUser'] = $document['name']['first_name'];
-	$userDetails['lastNameEditUser'] = $document['name']['last_name'];
-	$userDetails['emailEditUser'] = $document['email_address'];
-	$userDetails['addressLine1EditUser'] = $document['shipping_address']['address_line1'];
-	$userDetails['addressLine2EditUser'] = $document['shipping_address']['address_line2'];
-	$userDetails['cityEditUser'] = $document['shipping_address']['city'];
-	$userDetails['postcodeEditUser'] = $document['shipping_address']['postcode'];
+             if($connection){
 
-	echo json_encode($userDetails); // Return the information to the javascript
-	exit();
+                 //connecting to database
+                 $database=$connection->$database_name;
 
-}
+                 //connect to specific collection
+                 $collection=$databse->user;
 
-$errors = []; // Create an array to hold the errors
+                 $query=array('email'=>$email);
+                 //checking for existing user
+                 $count=$collection->findOne($query);
 
-// Sanitize Data
-$firstName =  filter_var($_POST['data']['firstNameEditUser'], FILTER_SANITIZE_STRING);
-$lastName =  filter_var($_POST['data']['lastNameEditUser'], FILTER_SANITIZE_STRING);
-$email =  strtolower(filter_var($_POST['data']['emailEditUser'], FILTER_SANITIZE_STRING));
-$addressLine1 =  filter_var($_POST['data']['addressLine1EditUser'], FILTER_SANITIZE_STRING);
-$addressLine2 =  filter_var($_POST['data']['addressLine2EditUser'], FILTER_SANITIZE_STRING);
-$city =  filter_var($_POST['data']['cityEditUser'], FILTER_SANITIZE_STRING);
-$postcode =  filter_var($_POST['data']['postcodeEditUser'], FILTER_SANITIZE_STRING);
-$password =  filter_var($_POST['data']['passwordEditUser'], FILTER_SANITIZE_STRING);
-$confirmPassword =  filter_var($_POST['data']['confirmPasswordEditUser'], FILTER_SANITIZE_STRING);
+                 if(!count($count)){
+                     //Save the New user
+                     $user=array('fname'=>$fname,'lname'=>$lname,'ScName'=>$ScName,'email'=>$email,'password'=>md5($password),'password'=>$password2);             
+                     $collection->save($user);
+                     echo "You are successfully registered.";
+                 }else{
+                     echo "Email is already existed.Please register with another Email id!.";
+                 }
 
+             }else{
 
-// Validate New Value with regular expressions and add an error if it doesn't match
-if (!preg_match("/^[-'a-zA-Z]+$/", $firstName))
-	$errors['firstNameEditUser'] = "Enter a valid first name";
+                  die("Database are not connected");
+             }
 
-if (!preg_match("/^[-'a-zA-Z]+$/", $lastName))
-	$errors['lastNameEditUser'] = "Enter a valid last name";
+        }else{
+            //Displaying the error
+            foreach($error as $err){
+                echo $err.'</br>';
+            }
+        }
+        }
 
-// Validate email address using built in filter
-if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-	$errors['emailEditUser'] = "Enter a valid email address";
-// Make sure the address line 1 isn't blank
-if ($addressLine1 == "")
-	$errors['addressLine1EditUser'] = "Enter a valid address line 1";
-// Make sure city isn't blank
-if ($city == "")
-	$errors['cityEditUser'] = "Enter a valid city";
+?>
 
-// Validate postcode using external API
-$postcodeLookup = file_get_contents("http://api.postcodes.io/postcodes/$postcode/validate");
-$postcodeLookup = json_decode($postcodeLookup);
-if (!$postcodeLookup->result)
-	$errors['postcodeEditUser'] = "Enter a valid UK postcode";
+<html>
+    <head>
+        <title>
+            Registration
+        </title>
+    </head>
+ <body>
+<form action="registration.php" method="POST">
+Screen Name:
+<input type="text" id="ScName" name="ScName"  /></br></br>
+First Name:
+<input type="text" id="fname" name="fname"  /></br></br>
+Last Name:
+<input type="text" id="lname" name="lname"  /></br></br>
+Email:
+<input type="text" id="email" name="email"  /></br></br>
+Password:
+<input type="password" id="password" name="password" />  </br></br>
+Confirm Password:
+<input type="password" id="password2" name="password2" />  </br></br>
 
-// See if password was changed and verify validity
-if (($password == "") and ($confirmPassword == "")) {
-	$passwordChange = false;
-} else {
-	$passwordChange = true;
-	if ($password != $confirmPassword) {
-		$errors['passwordEditUser'] = "The pasword you entered didn't match the confirm password field";
-		$errors['confirmPasswordEditUser'] = ""; // This is a dirty fix to make the box go red without displaying a message
-	} else {
-		$passwordHash = password_hash($password, PASSWORD_DEFAULT); // Hash the password
-	}
-}
-
-if (count($errors) > 0) { // If there were invalid fields
-	echo json_encode($errors); // Encode the messages in JSON to send back to the JS script
-    exit; // Don't execute any more of the script
-}
-
-if ($passwordChange) { // If the password was changed then
-	$collection->updateOne( // Update the db document with all of the new field values
-		['_id' => new MongoDB\BSON\ObjectId($_SESSION['userID'])],
-		['$set' => ['email_address' => $email, 
-		'password_hash' => $passwordHash,
-		'name.first_name' => $firstName,
-		'name.last_name' => $lastName,
-		'shipping_address.address_line1' => $addressLine1,
-		'shipping_address.address_line2' => $addressLine2,
-		'shipping_address.city' => $city,
-		'shipping_address.postcode' => $postcode]]
-		);
-} else { // Otherwisw
-	$collection->updateOne( // Update the db document with all of the new field values (except for the password)
-		['_id' => new MongoDB\BSON\ObjectId($_SESSION['userID'])],
-		['$set' => ['email_address' => $email, 
-		'name.first_name' => $firstName,
-		'name.last_name' => $lastName,
-		'shipping_address.address_line1' => $addressLine1,
-		'shipping_address.address_line2' => $addressLine2,
-		'shipping_address.city' => $city,
-		'shipping_address.postcode' => $postcode]]
-		);
-}
-
-echo json_encode(['result' => 'success']); // Return a success message to the javascript
-exit();
+<input  name="submit" id="submit" type="submit" value="register" />
+</form>
+    </body>
+</html>
